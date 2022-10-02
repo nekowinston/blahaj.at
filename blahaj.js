32871229585da35accd8acd3e684c483618499a9
Andreas <35840154+andreasgrafen@users.noreply.github.com>
2022-10-02 11:28:26 +0000

    feat: alpine haj (#1)
    * blahajlax
    
    * blahajlax deluxe
    
    * replace background

---
+++ blahaj.js
@@ -0,0 +1,19 @@
+document.addEventListener("mousemove", blahajlax)
+
+const bg  = document.querySelector('#background')
+const haj = document.querySelector('#blahaj')
+
+function blahajlax (event) {
+
+  let posBg  = bg.getAttribute('value')
+  let posHaj = haj.getAttribute('value')
+
+  let hajX   = (window.innerWidth  - event.pageX * posHaj) / 90
+  let hajY   = (window.innerHeight - event.pageY * posHaj) / 90
+  let bgX    = (window.innerWidth  - event.pageX * posBg) / 70
+  let bgY    = (window.innerHeight - event.pageY * posBg) / 70
+
+  bg.style.transform = `translateX(${bgX}px) translateY(${bgY}px)`
+  haj.style.transform = `translateX(${hajX}px) translateY(${hajY}px)`
+
+}
